3e145fa8c57c08afd84f42a20a6c2a878f1ae1c7
HENRY HIDEKI SAKAGUTI <110671070+hidekiaa@users.noreply.github.com>
2022-10-07 09:07:32 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,4 +28,19 @@ for(let j = i; j <= 10; j++){
 }
 }
  
- 
+ function total(){
+    
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+
+    for(let i=1; i <= t;i++){
+        r = v * (1+(j/100));
+        document.write("Mês " + i + "valor" + r + "<br>");
+        v = r; 
+    }
+     
+    document.write("Resutado:" + r);
+
+ }
