1d4a47e09bcadd61beffdd1a92300bf256ec5286
HENRY HIDEKI SAKAGUTI <110671070+hidekiaa@users.noreply.github.com>
2022-10-19 12:50:15 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -44,3 +44,33 @@ for(let j = i; j <= 10; j++){
     document.write("Resutado:" + r);
 
  }
+ function média(){
+    let n1 =document.getElementById("n1").value;
+    let n2 =document.getElementById("n2").value; 
+    let n3 =document.getElementById("n3").value;
+    let n4 =document.getElementById("n4").value;
+
+        let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4;
+        document.getElementById("resul").innerHTML = "mèdia  " + r;
+ }
+
+
+        function soma(){
+            let n1 =document.getElementById("n1").value;
+            let n2 =document.getElementById("n2").value; 
+            let n3 =document.getElementById("n3").value;
+            let n4 =document.getElementById("n4").value;
+        
+                let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4));
+                document.getElementById("resul").innerHTML = "soma  " + r;
+    }
+
+    function multi(){
+        let n1 =document.getElementById("n1").value;
+        let n2 =document.getElementById("n2").value; 
+        let n3 =document.getElementById("n3").value;
+        let n4 =document.getElementById("n4").value;
+    
+            let r = (Number(n1)*Number(n2)*Number(n3)*Number(n4));
+            document.getElementById("resul").innerHTML = "multi  " + r;
+    }
